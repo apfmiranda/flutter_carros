1d6d849f9c9b0bf2000fb4a0eb1656f6658312ab
apfmiranda <alexandre.miranda@gmail.com>
2020-04-17 17:52:19 -0300

    TabBar - listener

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:apfmiranda/pages/carro/carros_api.dart';
 import 'package:apfmiranda/pages/carro/carros_listView.dart';
 import 'package:apfmiranda/widgets/drawer_list.dart';
@@ -9,27 +8,44 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage>{
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+
+    _tabController.index = 1;
+    _tabController.addListener((){
+      print("tabb ${_tabController.index}");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Carros"),
-          bottom: TabBar(tabs: [
-            Tab(text: "Clássicos",),
-            Tab(text: "Esportivos",),
-            Tab(text: "Luxo",)
-          ]),
-        ),
-        body: TabBarView(children: [
-          CarrosListView(TipoCarro.classicos),
-          CarrosListView(TipoCarro.esportivos),
-          CarrosListView(TipoCarro.luxo)
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+        bottom: TabBar(controller: _tabController, tabs: [
+          Tab(
+            text: "Clássicos",
+          ),
+          Tab(
+            text: "Esportivos",
+          ),
+          Tab(
+            text: "Luxo",
+          )
         ]),
-        drawer: DrawerList(),
       ),
+      body: TabBarView(controller: _tabController, children: [
+        CarrosListView(TipoCarro.classicos),
+        CarrosListView(TipoCarro.esportivos),
+        CarrosListView(TipoCarro.luxo)
+      ]),
+      drawer: DrawerList(),
     );
   }
 }
