42e052402e5d0b83cbb8abc117d504e87993747e
apfmiranda <alexandre.miranda@gmail.com>
2020-04-23 20:41:40 -0300

    Actions: IconButton e PopupMenuButton

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -12,7 +12,29 @@ class CarroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.carro.nome)
+        title: Text(this.carro.nome),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMapa,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected:_onClickPopupMenu,
+            itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(value: "Editar", child: Text("Editar"),),
+              PopupMenuItem(value: "Deletar", child: Text("Deletar"),),
+              PopupMenuItem(value: "Share", child: Text("Share"),),
+            ];
+          },
+            
+          )
+
+        ],
       ),
       body: _body(),
     );
@@ -34,5 +56,25 @@ class CarroPage extends StatelessWidget {
           ),
         );
 
+  }
+
+  void _onClickMapa() {
+  }
+
+  void _onClickVideo() {
+  }
+
+  _onClickPopupMenu(String value) {
+    switch(value) {
+      case "Editar":
+        print("editar");
+        break;
+      case "Deletar":
+        print("deletar");
+        break;
+      case "Share":
+        print("Share");
+        break;
+    }
   }
 }
